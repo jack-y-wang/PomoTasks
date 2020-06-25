7b19286825199af41a9d70f8a4de2d7c3facd695
Jack Wang <jack.y.wang@berkeley.edu>
2020-06-24 23:40:43 -0700

    changed browser title

---
+++ build/precache-manifest.59391d518dbfe3a962de3866632aba1c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d20d2931b6dc374aa376",
+    "url": "/static/css/main.913e0949.chunk.css"
+  },
+  {
+    "revision": "d20d2931b6dc374aa376",
+    "url": "/static/js/main.51e653b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "db44082c39997cf5d4a5",
+    "url": "/static/js/2.9b8d35ff.chunk.js"
+  },
+  {
+    "revision": "2d87679670dcdf94ed49bdee4707d5e3",
+    "url": "/static/media/play.2d876796.svg"
+  },
+  {
+    "revision": "4cf8e40940b590fb7ad5966caa7565d6",
+    "url": "/static/media/pause.4cf8e409.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/static/media/x.b0200bf7.svg"
+  },
+  {
+    "revision": "10a5b071548379871a331ddae24e289f",
+    "url": "/static/media/check.10a5b071.svg"
+  },
+  {
+    "revision": "97582ec644cb63896869c761152ece1a",
+    "url": "/static/media/archive.97582ec6.svg"
+  },
+  {
+    "revision": "e631e62e6da051c160708cbe8fd553fb",
+    "url": "/static/media/drawkit-nature-man-colour.e631e62e.svg"
+  },
+  {
+    "revision": "fdf05e4fcdb76ffb98ace0d9c9b68972",
+    "url": "/index.html"
+  }
+];
